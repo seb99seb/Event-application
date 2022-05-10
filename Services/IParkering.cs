e83c61ead78441f65a458adac9a37d6b4cf80a5a
Christopher Sloth Vesterby <chri414g@edu.easj.dk>
2022-05-10 12:26:21 +0200

    Interface

---
+++ Services/IParkering.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Event_application.Services
+{
+    public interface IParkeringGeneric<T>
+    {
+
+        List<T> GetAll();
+
+        T GetbyId(int id);
+
+        // CRUD
+
+        T Create(T newParkering);
+
+        T Read();
+
+        T Update(T updateParkering);
+
+        T Delete(int id);
+
+    }
+}
