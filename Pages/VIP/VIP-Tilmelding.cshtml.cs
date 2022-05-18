e529088f2c78d4d8849ddf7a66c8150c1c4c5c93
seb99seb <sebvangpedersen@hotmail.dk>
2022-05-18 13:34:41 +0200

    VIP INTEGRATED INTO MASTER2

--- Pages/VIP/VIP-Tilmelding.cshtml.cs
+++ Pages/VIP/VIP-Tilmelding.cshtml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 using Event_application.Services;
 using Event_application.User;
@@ -11,43 +12,29 @@ namespace Event_application.Pages
 {
     public class VIP_TilmeldingModel : PageModel
     {
-        public int tilmedelser { get; set; }
-        private IParkeringGeneric<Event_application.Parkering> _service;
-        private BService _bservice;
-        private Bruger _bruger;
 
-        public VIP_TilmeldingModel(IParkeringGeneric<Event_application.Parkering> service, Bruger bruger, BService bService)
+        private TService _tservice;
+        private Bruger _bruger;
+        private BService _bservice;
+        public VIP_TilmeldingModel(TService ser, Bruger bruger, BService bService)
         {
-            _service = service;
+            _tservice = ser;
             _bruger = bruger;
             _bservice = bService;
         }
-        [BindProperty] public bool tilmeld { get; set; }
+
+
         public IActionResult OnGet()
         {
-            List<Event_application.Parkering> tilmelds = _service.GetAll();
-            List<Event_application.Parkering> Free = tilmelds.Where(p => p.BrugerID == -1).ToList();
-            tilmedelser = Free.Count;
-            tilmeld = _bruger.LoggedIn;
             return Page();
         }
+
         public IActionResult OnPost()
         {
-            List<Event_application.Parkering> tilmelds = _service.GetAll();
-            List<Event_application.Parkering> Free = tilmelds.Where(p => p.BrugerID == -1).ToList();
-            if (Free.Count > 0)
-            {
-                Event_application.Parkering p = Free[0];
-                p.BrugerID = _bservice.FindId(_bruger);
-                _service.Update(p);
-            }
-            tilmedelser = Free.Count;
+            int x = 5;
+            _tservice.Create(x);
             return Page();
         }
 
     }
 }
-
-
-
-
