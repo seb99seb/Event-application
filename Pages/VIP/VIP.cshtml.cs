fab5bd8b08460b7f3f0ff2e605c7f22a66f2a940
seb99seb <sebvangpedersen@hotmail.dk>
2022-05-18 13:21:50 +0200

    WIP2

--- Pages/VIP/VIP.cshtml.cs
+++ Pages/VIP/VIP.cshtml.cs
@@ -9,23 +9,36 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Event_application.Pages.VIP
 {
+    /// <summary>
+    ///  opretter alle cataloger til siden i event applikationen
+    /// </summary>
     public class VIPModel : PageModel
     {
-        private Catalog catalogA;
+        private CatalogA catalogA;
         private CatalogV catalogve;
         private CatalogC catalogCh;
 
+        /// <summary>
+        /// Henter alle cataloger til siden i event applikationen
+        /// </summary>
         public VIPModel()
         {
-            catalogA = new Catalog();
+            catalogA = new CatalogA();
             catalogve = new CatalogV();
             catalogCh = new CatalogC();
         }
 
+        /// <summary>
+        /// Henter alle lister til event applikationen
+        /// </summary>
         public Dictionary<int, Almindelig> al { get; private set; }
         public Dictionary<int, Vegansk> ve { get; private set; }
         public Dictionary<int, Champagne> ch { get; private set; }
 
+        /// <summary>
+        /// Kalder og henter alle lister således, at listerne kan ses
+        /// </summary>
+        /// <returns>siden med lister</returns>
         public IActionResult OnGet()
         {
             al = catalogA.AllA();
