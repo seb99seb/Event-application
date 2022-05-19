17666822bc4b6f22d177fa12ea999b05cecd0bd7
Rúni Bjartalið <runi0204@edu.zealand.dk>
2022-05-19 16:57:42 +0200

    Små justeringer i menuen

--- VIP-Menuer/CatalogC.cs
+++ VIP-Menuer/CatalogC.cs
@@ -15,9 +15,9 @@ namespace Event_application.VIP_Menuer
         public CatalogC()
         {
             champagnes = new Dictionary<int, Champagne>();
-            champagnes.Add(1, new Champagne() { Id = 1, Navn = "Fransk champagne", Beskrivelse = "Smager godt samme med......", Vol = "20%", Pris = 100, Billede = "Champagne 1.jpg" });
-            champagnes.Add(2, new Champagne() { Id = 2, Navn = "Italiensk champagne", Beskrivelse = "Smager godt samme med......", Vol = "15%", Pris = 90, Billede = "Champagne 2.jpg" });
-            champagnes.Add(3, new Champagne() { Id = 3, Navn = "Græsk", Beskrivelse = "Smager godt samme med......", Vol = "10%", Pris = 120, Billede = "Champagne 3.jpg" });
+            champagnes.Add(1, new Champagne() { Id = 1, Navn = "Fransk champagne", Beskrivelse = "Er god til......", Vol = "20%", Pris = 100, Billede = "Champagne 1.jpg" });
+            champagnes.Add(2, new Champagne() { Id = 2, Navn = "Italiensk champagne", Beskrivelse = "Er god til......", Vol = "15%", Pris = 90, Billede = "Champagne 2.jpg" });
+            champagnes.Add(3, new Champagne() { Id = 3, Navn = "Græsk CHAMPAGNE", Beskrivelse = "Er god til......", Vol = "10%", Pris = 120, Billede = "Champagne 3.jpg" });
         }
 
         public Dictionary<int, Champagne> AllCh()
