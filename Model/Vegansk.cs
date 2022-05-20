ba3c6b81a61fc99091838ba608a838c26fbb4196
Rúni Bjartalið <runi0204@edu.zealand.dk>
2022-05-20 10:50:35 +0200

    vip4

--- Model/Vegansk.cs
+++ Model/Vegansk.cs
@@ -6,40 +6,119 @@ using System.Threading.Tasks;
 
 namespace Event_application.Model
 {
+
     /// <summary>
-    /// En model klasse - Vegansk indeholder konstruktører, properties,
-    /// defult konstruktører og ToString metode. Vegansk klassen arver fra Menuvare klassen
+    /// Model klassen indholder id, navn, beskrivelse, pris og billede
     /// </summary>
-    public class Vegansk : Menuvare
+    public class Vegansk
     {
+        /// <summary>
+        /// Instandsfelter, som har til formål at gemme værdierne id, navn, beskrivelse, pris og billede
+        /// </summary>
+        private int _id;
+        private string _navn;
+        private string _beskrivelse;
+        private double _pris;
+        private string _billede;
 
 
         /// <summary>
-        /// Konstruktørerne er dem der laver objekterne, id, navn, beksrivelse, pris og billede uden værdier
+        /// Konstruktørerne er dem der laver følgende objekter, uden værdier 
         /// </summary>
+        /// <param name="id">Id er nummer i på en ret den veganske menu</param>
+        /// <param name="navn"> Navn på retter i den veganske menu</param>
+        /// <param name="beskrivelse"> En kort beskrivelse om retterne i den veganske menu </param>
+        /// <param name="pris">Viser prisen på retter i den veganske menu </param>
+        /// <param name="billede"> viser billeder af retter i den veganske menu en</param>
         public Vegansk(int id, string navn, string beskrivelse, double pris, string billede)
         {
-    
+            _id = id;
+            _navn = navn;
+            _beskrivelse = beskrivelse;
+            _pris = pris;
+            _billede = billede;
         }
 
-       
 
         /// <summary>
-        /// Defult konstruktørerne - med værdier
+        /// Properties dem, der henter og sætter id, navn, beskrivelse, pris og billede
+        /// </summary>
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Navn
+        {
+            get => _navn;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Navn skal være længere end 3 tegn lang");
+
+                }
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Navn skal være længere end 3 tegn lang");
+                }
+                _navn = value;
+            }
+        }
+
+        public string Beskrivelse
+        {
+            get => _beskrivelse;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Beskrivelse skal være mindt 10 tegn lang ");
+
+                }
+                if (value.Length < 10)
+                {
+                    throw new ArgumentException("Beskrivelse skal være 10 teg lang");
+                }
+                _beskrivelse = value;
+            }
+        }
+
+        public double Pris
+        {
+            get => _pris;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(" Prisen skal være null eller posetiv");
+                }
+                _pris = value;
+            }
+        }
+
+        public string Billede
+        {
+            get => _billede;
+            set => _billede = value;
+        }
+
+        /// <summary>
+        /// Defult konstruktørerne er dem laver laver objekterne med værdier
         /// </summary>
         public Vegansk() : this(2, "Vegansk Burger", "tomas, agurk, salat og grønne linser", 45, "burgerBillede")
         {
         }
 
-        /// <summary>
-        /// ToString metoden gennem læser id, navn, beskrivelse, pris og billede
-        /// </summary>
-        /// <returns>Returnere id, navn, beskrivelse, pris og billede i systemet</returns>
 
+        /// <summary>
+        /// ToString metoden er til at gennemlæse alle objekterne igennem: id, navn, beskrivelse, pris og billede
+        /// </summary>
+        /// <returns>Returnere alle objekterne i systemet</returns>
         public override string ToString()
         {
-            return $"{nameof(Id)}: {Id}, {nameof(Navn)}: {Navn}, {nameof(Beskrivelse)}: {Beskrivelse}, {nameof(Pris)}: {Pris}, {nameof(Billede)}: {Billede}";
+            return $"{nameof(_id)}: {_id}, {nameof(_navn)}: {_navn}, {nameof(_beskrivelse)}: {_beskrivelse}, {nameof(_pris)}: {_pris}, {nameof(_billede)}: {_billede}";
         }
-
     }
 }
