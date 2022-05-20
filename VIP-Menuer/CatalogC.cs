7ac04bc50af33d2a3fa6d08a23ab33934cb6c7d8
Rúni Bjartalið <runi0204@edu.zealand.dk>
2022-05-20 10:53:12 +0200

    ca2

--- VIP-Menuer/CatalogC.cs
+++ VIP-Menuer/CatalogC.cs
@@ -8,44 +8,45 @@ using Event_application.Model;
 namespace Event_application.VIP_Menuer
 {
     /// <summary>
-    ///  Klassen indholder en Dictionatry liste med id, navn, beskrivelse, pris og billede
+    /// Model klassen indholder en Dictionatry liste med id, navn, beskrivelse, pris og billede
     /// </summary>
+
     public class CatalogC
     {
 
         /// <summary>
-        /// Henter objekter fra model klassen "Champagne" til en dictionary, ved at bruge af get
+        /// Henter objekter fra model klassen "champagne" til en dictionary, ved at bruge get
         /// </summary>
-
 
         private Dictionary<int, Champagne> champagnes { get; }
 
-
         /// <summary>
-        /// Objekterne fra model klassen "Champagne bliver hentet og sæt op til en liste af typen dictionary
+        /// Objekterne fra model klassen "champagne" bliver hentet og sæt op til en liste af typen dictionary
         /// </summary>
+
         public CatalogC()
         {
             champagnes = new Dictionary<int, Champagne>();
-            champagnes.Add(1, new Champagne() { Id = 1, Navn = "Fransk champagne", Beskrivelse = "God til vegansk burger", Vol = "20%", Pris = 100, Billede = "Champagne 1.jpg" });
-            champagnes.Add(2, new Champagne() { Id = 2, Navn = "Italiensk champagne", Beskrivelse = "God til er pizza ", Vol = "15%", Pris = 90, Billede = "Champagne 2.jpg" });
-            champagnes.Add(3, new Champagne() { Id = 3, Navn = "Græsk champagne", Beskrivelse = "God til pitabrød", Vol = "10%", Pris = 120, Billede = "Champagne 3.jpg" });
+            champagnes.Add(1, new Champagne() { Id = 1, Navn = "Fransk champagne", Beskrivelse = "Smager godt samme med......", Vol = "20%", Pris = 100, Billede = "Champagne 1.jpg" });
+            champagnes.Add(2, new Champagne() { Id = 2, Navn = "Italiensk champagne", Beskrivelse = "Smager godt samme med......", Vol = "15%", Pris = 90, Billede = "Champagne 2.jpg" });
+            champagnes.Add(3, new Champagne() { Id = 3, Navn = "Græsk", Beskrivelse = "Smager godt samme med......", Vol = "10%", Pris = 120, Billede = "Champagne 3.jpg" });
         }
+
 
 
         /// <summary>
         /// Metoden til at hente alle objekter og værdier i listen
         /// </summary>
-        /// <returns>Hvis hvis champagne listen er tom bliver den returneret</returns>
+        /// <returns>Hvis champagne listen er tom bliver den returneret</returns>
         public Dictionary<int, Champagne> AllCh()
         {
             return champagnes;
         }
 
         /// <summary>
-        /// Opretter en ny Champagne til listen eller systemet
+        /// Opretter en ny champagne til listen eller systemet
         /// </summary>
-        /// <param name="al">Det nye objekt i Champagne listen tilføjes eller add</param>
+        /// <param name="al">Det nye objekt bliver tilføjet eller add</param>
         public void AddA(Champagne ch)
         {
             champagnes.Add(ch.Id, ch);
